445939047bb6cc3c7cd48aa34209e8eac31ff3a6
Yeon Jin Lee <yeonjin.lee@nasa.gov>
2012-06-28 16:04:13 -0700

    transparencty for preview added

--- geocamTiePoint/static/geocamTiePoint/overlay-preview.js
+++ geocamTiePoint/static/geocamTiePoint/overlay-preview.js
@@ -1,5 +1,8 @@
 var tileSize=256;
 var offset=3;
+var opac = 0.4;
+var OPACITY_MAX_PIXELS = 57; 
+var imageOverlay;
 
 // find max zoom
 var maxZoom = 0; var offset = 3;
@@ -8,6 +11,7 @@ if (overlay['imageSize'][0] > overlay['imageSize'][1]) {
 } else {
     maxZoom = Math.ceil(Math.log(overlay['imageSize'][1] / tileSize, 2)) + offset;
 }
+
 
 function getTransformedImageTileUrl(coord,zoom) {
     var normalizedCoord = getNormalizedCoord(coord,zoom);
@@ -25,10 +29,12 @@ var transformedImageMapTypeOptions = {
     maxZoom: maxZoom,
     minZoom: offset,
     radius: 1738000,
+    opacity: parseFloat(opac),
     name: "transformed-image-map"
 }
 
 var transformedImageMapType = new google.maps.ImageMapType(transformedImageMapTypeOptions);
+
 
 var map;
 var currCity = new google.maps.LatLng(0,0);//figure this out form tie points
@@ -43,7 +49,14 @@ function initialize() {
 
     //insert the overlay map as first overlay map type at position 0
     map.overlayMapTypes.insertAt(0, transformedImageMapType);
+
+    var initialOpacity = 40;
+   // imageOverlay = new CustomTileOverlay(map,initialOpacity);
+   // imageOverlay.show();
+
+    createOpacityControl(map,initialOpacity);
 }
+
 
 function getNormalizedCoord(coord, zoom) {
     var y = coord.y;
@@ -63,6 +76,80 @@ function getNormalizedCoord(coord, zoom) {
     };
 }
 
-function showValue(newValue) {
-    document.getElementById("range").innerHTML=newValue;
+//set up a transparency slider
+function createOpacityControl(map, opacity) {
+    var sliderImageUrl =  data_url+"geocamTiePoint/images/opacity-slider3d6.png";
+
+    // Create main div to hold the control.
+    var opacityDiv = document.createElement('DIV');
+    opacityDiv.setAttribute("style", "margin:5px;overflow-x:hidden;overflow-y:hidden;background:url(" + sliderImageUrl + ") no-repeat;width:71px;height:21px;cursor:pointer;");
+
+    // Create knob
+    var opacityKnobDiv = document.createElement('DIV');
+    opacityKnobDiv.setAttribute("style", "padding:0;margin:0;overflow-x:hidden;overflow-y:hidden;background:url(" + sliderImageUrl + ") no-repeat -71px 0;width:14px;height:21px;");
+    opacityDiv.appendChild(opacityKnobDiv);
+
+    var opacityCtrlKnob = new ExtDraggableObject(opacityKnobDiv, {
+                            restrictY: true,
+                            container: opacityDiv
+    });
+
+    google.maps.event.addListener(opacityCtrlKnob, "dragend", function() {
+            setOpacity(opacityCtrlKnob.valueX());
+    });
+
+    google.maps.event.addDomListener(opacityDiv, "click", function(e) {
+        var left = findPosLeft(this);
+        var x = e.pageX - left - 5;
+        opacityCtrlKnob.setValueX(x);
+        setOpacity(x);
+    });
+
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(opacityDiv);
+    var initialValue = OPACITY_MAX_PIXELS / (100 / opacity);
+    opacityCtrlKnob.setValueX(initialValue);
+    setOpacity(initialValue);
 }
+
+
+function setOpacity(pixelX) {
+    //Range 0 to OPACITY_MAX_PIXELS
+    var value = (100 / OPACITY_MAX_PIXELS)* pixelX;
+    if (value < 0) value = 0;
+    if (value ==0) {
+    } else {
+        transformedImageMapType.setOpacity((100 / OPACITY_MAX_PIXELS) * pixelX /100);
+        console.log("opacity : "+(100 / OPACITY_MAX_PIXELS) * pixelX)/100;
+    }   
+}
+
+
+/*
+function setOpacity(pixelX) {
+    // Range = 0 to OPACITY_MAX_PIXELS
+    var value = (100 / OPACITY_MAX_PIXELS) * pixelX;
+    if (value < 0) value = 0;
+    if (value == 0) {
+        if (imageOverlay.visible == true) {
+            imageOverlay.hide();
+        }
+    }
+    else {
+        imageOverlay.setOpacity(value);
+        if (imageOverlay.visible == false) {
+            imageOverlay.show();
+        }
+    }
+}
+*/
+function findPosLeft(obj) {
+    var curleft = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft +=obj.offsetLeft;
+        } while (obj = obj.offsetParent);
+        return curleft;
+    }
+    return undefined;
+}
+
