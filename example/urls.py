5069d1e2a5cd1931046440d7113943936f6a824b
Yeon Jin Lee <yeonjin.lee@nasa.gov>
2012-06-29 15:24:42 -0700

    urls.py modified

--- example/urls.py
+++ example/urls.py
@@ -12,12 +12,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 import geocamTiePoint
+from geocamTiePoint.urls import urlpatterns as geocamTiePointPatterns
 
 urlpatterns = patterns('',
     # Example:
     # (r'^example/', include('example.foo.urls')),
-    (r'^'+geocamTiePoint.settings.TIEPOINT_URL+r'/',
-     include('geocamTiePoint.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
@@ -27,6 +26,8 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 
 )
+
+urlpatterns += geocamTiePointPatterns
 
 urlpatterns = urlpatterns + patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
