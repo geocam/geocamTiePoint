3a65cb543f70cb24eb0f3185f3e23ccf5828387b
Ted Scharff <e.b.scharff@nasa.gov>
2012-08-09 02:20:45 -0700

    WIP: remove quadtree related stuff from views.py to quadtree.py

--- geocamTiePoint/models.py
+++ geocamTiePoint/models.py
@@ -7,9 +7,10 @@
 from django.db import models
 from geocamTiePoint import settings
 from django.core.files.storage import FileSystemStorage
-from jsonfield import JSONField
-
 import os, shutil
+
+from jsonfield import JSONField
+from quadtree import makeQuadTree
 
 dataStorage = FileSystemStorage(location=settings.DATA_ROOT)
 
@@ -23,7 +24,8 @@ class Overlay(models.Model):
     imageType = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     key = models.AutoField(primary_key=True, unique=True)
-    last_quadtree = models.ForeignKey(Overlay, blank=True)
+    unaligned_quadtree = models.ForeignKey(QuadTree, blank=True)
+    aligned_quadtree = models.ForeignKey(QuadTree, blank=True)
 
     class Meta:
         ordering = ['-key']
@@ -40,10 +42,11 @@ class Overlay(models.Model):
         super(self, Overlay).delete(*args, **kwargs)
 
     def generateQuadTree(self):
-        self.last_quadtree = QuadTree(overlay=self))
+        self.unaligned_quadtree = QuadTree(overlay=self))
+        self.unaligned_quadtree.generate()
 
 class QuadTree(models.Model):
-    overlay = models.ForeignKey(Overlay, on_delete=Models.SET_NULL) # avoid circular FK constraints making deletes impossible
+    overlay = models.ForeignKey(Overlay, null=True, on_delete=Models.SET_NULL) # avoid circular FK constraints making deletes impossible
     tiles = JSONField(null=True) # notionally a long array of tile storage keys
 
     def delete(self, *args, **kwargs):
@@ -68,27 +71,5 @@ class QuadTree(models.Model):
         image = PIL.Image( dataStorage.Open( self.overlay.image ) )
         coords = ((0,0),(image.size[0],0),(0,image.size[1]),image.size)
         basePath = 'quadtree/' + str(self.id)
-
-        if image.size[0] > image.size[1]:
-            maxZoom = int(math.ceil(math.log(image.size[0]/TILE_SIZE,2)))
-        else:
-            maxZoom = int(math.ceil(math.log(image.size[1]/TILE_SIZE,2)))
-        for i in xrange(maxZoom, -1, -1):
-            nx = int(math.ceil(image.size[0]/TILE_SIZE))
-            ny = int(math.ceil(image.size[1]/TILE_SIZE))
-            for ix in xrange(nx):
-                for iy in xrange(ny):
-                    if testOutsideImage((TILE_SIZE*ix,TILE_SIZE*iy),coords) and\
-                            testOutsideImage((TILE_SIZE*(ix+1),TILE_SIZE*iy),coords) and\
-                            testOutsideImage((TILE_SIZE*ix,TILE_SIZE*(iy+1)),coords) and\
-                            testOutsideImage((TILE_SIZE*(ix+1),TILE_SIZE*(iy+1)),coords):
-                        continue
-                    if not os.path.exists(basePath+'/%s/%s/' % (i+ZOOM_OFFSET,ix)):
-                        os.makedirs(basePath+'/%s/%s' % (i+ZOOM_OFFSET,ix))
-                    corners = [TILE_SIZE*ix,TILE_SIZE*iy,TILE_SIZE*(ix+1),TILE_SIZE*(iy+1)]
-                    corners = [int(round(x)) for x in corners]
-                    newImage = image.crop(corners)
-                    newImage.save(basePath+'/%s/%s/%s.jpg' % (i+ZOOM_OFFSET,ix,iy))
-            image = image.resize((int(math.ceil(image.size[0]/2.)),
-                                  int(math.ceil(image.size[1]/2.))),
-                                 Image.ANTIALIAS)
+        
+        tile_keys = makeQuadTree( image, coords, basePath, dataStorage)
