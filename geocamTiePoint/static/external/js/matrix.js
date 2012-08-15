9b06bd4f89a70c9890908fc95d26a98a7efe4365
Trey Smith <trey.smith@nasa.gov>
2012-08-15 14:18:15 -0700

    consolidated optimization code into fewer files

--- geocamTiePoint/static/external/js/matrix.js
+++ geocamTiePoint/static/external/js/matrix.js
@@ -1,8 +1,4 @@
-// __BEGIN_LICENSE__
-// Copyright (C) 2008-2010 United States Government as represented by
-// the Administrator of the National Aeronautics and Space Administration.
-// All Rights Reserved.
-// __END_LICENSE__
+// __NO_RELICENSE__
 
 /**
  * Generic matrix class. Built for readability, not for speed.
