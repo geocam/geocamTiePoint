caae356abaa10f17f4707b67da7fbc03552a507e
Jerome Rasky <jyrome.112@gmail.com>
2012-08-15 14:51:38 -0700

    Added a url to accommodate new view that work has begun on. Successfully made the two transition by using a workaround.

--- geocamTiePoint/static/geocamTiePoint/js/router.js
+++ geocamTiePoint/static/geocamTiePoint/js/router.js
@@ -3,13 +3,24 @@ App.router = Ember.Router.create({
     root: Ember.Route.extend({
 	index: Ember.Route.extend({
 	    route: '/',
-	    redirectsTo: 'alignTiePoints'
+	    redirectsTo: 'placeTiePoints'
 	}),
 	alignTiePoints: Ember.Route.extend({
-	    route: '/:overlay',
+	    route: '/align/:overlay',
 	    connectOutlets: function(router, controller) {
 		App.AlignTiePoints.setOverlay(controller.overlay);
 		router.get('applicationController').connectOutlet('alignTiePoints', controller);
+	    }
+	}),
+	placeTiePoints: Ember.Route.extend({
+	    route: '/place/:overlay',
+	    nextStep: function() {
+		App.router.transitionTo('alignTiePoints', App.PlaceTiePoints.controller);
+	    },
+	    connectOutlets: function(router, controller) {
+		App.PlaceTiePoints.setOverlay(controller.overlay);
+		App.PlaceTiePoints.setController(controller);
+		router.get('applicationController').connectOutlet('placeTiePoints', controller);
 	    }
 	})
     })
--- geocamTiePoint/static/geocamTiePoint/js/views/PlaceTiePoints.js
+++ geocamTiePoint/static/geocamTiePoint/js/views/PlaceTiePoints.js
@@ -1,13 +1,16 @@
-App.PlacetiePointsHandlebars = '<div class="page-title">Overlay number: {{App.PlaceTiePoints.overlay}}</div>';
+App.PlaceTiePointsHandlebars = '<div class="page-title">Overlay number: {{App.PlaceTiePoints.overlay}}</div><br><div class="action-buttons"><input type="button" value="Help"></input><input type="button" value="Add Tie Point"></input></div><br><div id="place-tie-points-map-canvas"></div><br><input type="button" value="Align Tie Points" {{action nextStep}}></input>';
 
 App.PlaceTiePoints = Ember.Object.create({
-    overlay: null,
+    overlay: null, controller: null,
     setOverlay: function (overlay) {
 	this.overlay = overlay;
+    },
+    setController: function(controller) {
+	this.controller = controller;
     }
 });
 
 App.PlaceTiePointsController = Ember.Controller.extend();
-App.AlignTiePointsview = Ember.Viewextend({
+App.PlaceTiePointsView = Ember.View.extend({
     template: Ember.Handlebars.compile(App.PlaceTiePointsHandlebars)
 });
