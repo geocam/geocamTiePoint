77255e7da7d2c3d0df7709c34b3fbfa473fb53b6
Gabino Dabdoub <gdabdoub3@gatech.edu>
2012-08-16 15:59:18 -0700

    overlay-view.js moved functions

--- geocamTiePoint/static/geocamTiePoint/js/overlay-view.js
+++ geocamTiePoint/static/geocamTiePoint/js/overlay-view.js
@@ -231,12 +231,6 @@ function initialize_image() {
     }
 }
 
-function fitNamedBounds(b) {
-    var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(b.south, b.west),
-                                              new google.maps.LatLng(b.north, b.east));
-    map.fitBounds(bounds);
-}
-
 function handleNoGeolocation(errorFlag) {
     fitNamedBounds(settings.GEOCAM_TIE_POINT_DEFAULT_MAP_VIEWPORT);
 }
@@ -594,23 +588,6 @@ function actionPerformed() {
 	    undoStack.push(redoStack.pop());
     }
     pushState(undoStack);
-}
-
-function latLonToMeters(latLon) {
-    var mx = latLon.lng() * originShift / 180;
-    var my = Math.log(Math.tan((90 + latLon.lat()) * Math.PI / 360)) /
-	(Math.PI / 180);
-    my = my * originShift / 180;
-    return {x:mx,
-	    y:my};
-}
-
-function metersToLatLon(meters) {
-    var lng = meters.x * 180 / originShift;
-    var lat = meters.y * 180 / originShift;
-    lat = ((Math.atan(Math.exp((lat * (Math.PI / 180)))) * 360) / Math.PI) - 90;
-    var latLng = new google.maps.LatLng(lat,lng);
-    return latLng;
 }
 
 function metersToPixels(meters) {
