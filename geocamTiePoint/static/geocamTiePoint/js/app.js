b2677b8d46dbdef3a0c4cbf50da03a39e92660ab
Ted Scharff <e.b.scharff@nasa.gov>
2012-08-24 00:07:36 -0700

    WIP: AlignOverlay skeleton with view-oriented controller style.

--- geocamTiePoint/static/geocamTiePoint/js/app.js
+++ geocamTiePoint/static/geocamTiePoint/js/app.js
@@ -38,23 +38,59 @@ App.OverlaylistView = Ember.View.extend({
     overlaysBinding: 'App.OverlayController.content',
 });
 
+App.OverlayAlignView = Ember.View.extend({
+    templateName: 'overlay_align',
+    map_container_id: 'map_canvas',
+    didInsertElement: function() {
+        var map_container = $('#'+this.map_container_id);
+        var overlay = this.content;
+        var mapOptions = {
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        };
+        map = new google.maps.Map(map_container[0], mapOptions);
+        fitNamedBounds(overlay.bounds);
+    },
+
+});
 
 
 /**************************
 * Controllers
 **************************/
-App.ApplicationController = Ember.Controller.extend();
+App.ApplicationController = Em.Controller.extend();
 
-App.OverlayListController = Ember.ArrayController.extend({
+App.OverlayController = Em.ArrayController.extend({
     content: [],
+    initialized: false,
+    initializing: false,
     init: function() {
         var me = this;
+        this.initializing = true;
+        this.loadOverlays(function(){ 
+            me.initialized = true; 
+            console.log('overlayController initialized.');
+        });
+    },
+
+    loadOverlays: function(callback) {
         $.getJSON('/overlays.json', function(data) {
             if ( me.content ) { me.set('content', []); }
             $.each(data, function(idx, item) {
                 overlay = App.Overlay.create( item );
                 me.pushObject(overlay);
             });
+            if (callback) { callback(); }
+        });
+    },
+    getByKey: function(key) {
+        if (! this.initialized) {
+            if (! this.initializing ) { this.init() };
+        }
+        // Return the overlay from this collection matching the given key
+        $.each( this.content, function(idx, overlay) {
+            if (overlay.key === key) {
+                return overlay;
+            }
         });
     },
 });
--- geocamTiePoint/static/geocamTiePoint/js/router.js
+++ geocamTiePoint/static/geocamTiePoint/js/router.js
@@ -10,26 +10,52 @@ App.Router = Ember.Router.extend({
 
         listOverlays: Ember.Route.extend({
             route: '/overlays/',
+            alignOverlayAction: Em.Route.transitionTo('alignOverlay'),
+            
             connectOutlets: function(router, controller) {
                 router.get('applicationController').connectOutlet( {
+                    /* Ember intends a magic name-to-controller-and-view mechanism,
+                     * where you can give connectOutlet a name string and it will infer
+                     * the correct controller instance and view class to use,
+                     * but I haven't gotten it to work properly.  
+                     * Luckily, there are overrides. --EBS
+                    */ 
                     controller: App.OverlayListController.create(),
                     viewClass: App.OverlaylistView,
                 } );
             },
         }),
 
-        /*
+        alignOverlay: Ember.Route.extend({
+            route: '/overlay/:overlay_key/align',
+            serialize: function(router, context) {
+                return {overlay_key: context.key}
+            },
+            deserialize: function(router, params) {
+                return router.get('overlayController').getByKey(params.overlay_key);
+            },
+            connectOutlets: function(router, overlay) {
+                console.log("context for overlayAlignController");
+                console.log(overlay);
+                router.get('applicationController').connectOutlet({
+                    controller: router.get('overlayController'),
+                    viewClass: App.OverlayAlignView,
+                });
+            },
+        }),
+
         alignTiePoints: Ember.Route.extend({
             route: '/align/:overlay',
-            connectOutlets: function(router, controller) {
-
-            App.AlignTiePoints.setOverlay(controller.overlay);
+            connectOutlets: function(router, context) {
+                var overlay_id = context.overlay;
+                App.AlignTiePoints.setOverlay(context.overlay);
                 (router
-                 .get('applicationController')
-                 .connectOutlet('alignTiePoints', controller));
-
-            }
+                     .get('applicationController')
+                     .connectOutlet('alignTiePoints', context.overlay)
+                 );
+            },
         }),
+        /*
 
         placeTiePoints: Ember.Route.extend({
             route: '/place/:overlay',
