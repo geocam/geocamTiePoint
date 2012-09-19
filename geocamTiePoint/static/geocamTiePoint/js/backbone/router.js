e022641429bc44ac5b1f25907745f011cf8bd02d
Trey Smith <trey.smith@nasa.gov>
2012-09-19 15:31:46 -0700

    fixed gjslint warnings

--- geocamTiePoint/static/geocamTiePoint/js/backbone/router.js
+++ geocamTiePoint/static/geocamTiePoint/js/backbone/router.js
@@ -1,36 +1,36 @@
 var app = app || {};
 
-$(function ($) {
+$(function($) {
     var AppRouter = Backbone.Router.extend({
         routes: {
             'overlays/': 'listOverlays',
             'overlay/:overlay_id': 'showOverlay',
-            '': 'root',
+            '': 'root'
         },
-        
+
         root: function() {
             this.navigate('overlays/', {trigger: true});
         },
-        
+
         listOverlays: function() {
             console.log('Routed to listOverlays.');
             var view = new app.views.ListOverlaysView();
             view.render();
         },
-        
+
         showOverlay: function(overlay_id) {
             console.log('Routed to showOverlay for ' + overlay_id);
-            //var view = new app.views.ImageQtreeView( {id: overlay_id} );
-            var view = new app.views.SplitOverlayView( {id: overlay_id} );
+            //var view = new app.views.ImageQtreeView({id: overlay_id});
+            var view = new app.views.SplitOverlayView({id: overlay_id});
             view.render();
         },
 
 
-        start: function(){
+        start: function() {
             Backbone.history.start();
-        }, 
+        }
     });
 
     app.router = new AppRouter();
-    
+
 });
