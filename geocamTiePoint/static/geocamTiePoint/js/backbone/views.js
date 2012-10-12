132a4684859c5a8c390c191aa135399cb56e4d22
Ted Scharff <e.b.scharff@nasa.gov>
2012-10-12 12:05:58 -0700

    Tidied up some indentation.

--- geocamTiePoint/static/geocamTiePoint/js/backbone/views.js
+++ geocamTiePoint/static/geocamTiePoint/js/backbone/views.js
@@ -630,20 +630,20 @@ $(function($) {
             this.$('input#newOverlayFormSubmitButton').click(this.submitForm);
         },
 
-            getCookie: function(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie != '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = $.trim(cookies[i]);
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
+        getCookie: function(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = $.trim(cookies[i]);
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
                     }
                 }
-                return cookieValue;
-            },
+            }
+            return cookieValue;
+        },
 
         csrfSafeMethod: function(method) {
             return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
